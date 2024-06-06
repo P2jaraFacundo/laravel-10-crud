224ca39af20769d92fb33034c75ee2fac16aa55a
P2jaraFacundo <facujara1005@gmail.com>
2024-06-06 16:25:37 -0300

    Update 2024_05_22_193748_create_parameters_table.php

--- database/migrations/2024_05_22_193748_create_parameters_table.php
+++ database/migrations/2024_05_22_193748_create_parameters_table.php
@@ -3,28 +3,42 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
+class CreateParametersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('parameters', function (Blueprint $table) {
-            $table->id();            
+            $table->id();
             $table->integer('class_days');
             $table->integer('promotion_percentage');
             $table->integer('regular_percentage');
             $table->timestamps();
         });
+
+        // Insertar datos por defecto
+        DB::table('parameters')->insert([
+            'class_days' => 180,
+            'promotion_percentage' => 75,
+            'regular_percentage' => 50,
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('parameters');
     }
-};
+}
